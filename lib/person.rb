e1e849a16e84d9f80869c07196145561b4972c45
michaelstsnow <michael.st.snow@gmail.com>
2018-06-15 01:03:45 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
   def name=(person_name)
-    @this_persons_name=person_name
+    @name=person_name
   end
 
   def name
-    @this_persons_name
+    @name
   end
 
   def job=(person_job)
