32ae9d37ac8433fc3e27dccbb4582180b4030da9
michaelstsnow <michael.st.snow@gmail.com>
2018-06-15 01:02:11 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class person
   def name=(person_name)
-    this_persons_name=person_name
+    @this_persons_name=person_name
   end
 
   def name
-    this_persons_name
+    @this_persons_name
   end
 
   def job=(person_job)
-    this_persons_job=person_job
+    @this_persons_job=person_job
   end
 
   def job
-    this_persons_job
+    @this_persons_job
   end
 end
