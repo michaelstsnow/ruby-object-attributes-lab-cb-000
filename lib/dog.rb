af5ba8846a87a3a5bfd1a9e23b05e16da87ddc85
michaelstsnow <michael.st.snow@gmail.com>
2018-06-15 00:59:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
-class person
-  def name=(person_name)
-    this_persons_name=person_name
+class dog
+  def name=(dog_name)
+    this_dogs_name=dog_name
   end
 
   def name
-    this_persons_name
+    this_dogs_name
   end
 end
